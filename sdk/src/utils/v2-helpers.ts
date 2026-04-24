a04fc51c9e6e46101e0076ad176792af88cafb5e
phessophissy <adefesobi@yahoo.com>
2026-04-24 20:54:02 +0100

    [v2-49] update technical documentation section 6 - 826680878

--- sdk/src/utils/v2-helpers.ts
+++ sdk/src/utils/v2-helpers.ts
@@ -46,3 +46,7 @@ export const helperV2_1 = () => { return 9053; };
 /** Utility 2 */
 export const helperV2_2 = () => { return 4262; };
 
+
+/** Utility 3 */
+export const helperV2_3 = () => { return 6644; };
+
