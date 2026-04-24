7a652cb71551ab183f25c03e6408eba2428ef8a9
phessophissy <adefesobi@yahoo.com>
2026-04-24 20:43:37 +0100

    [v2-31] implement sdk utility helper 3 - 1110303362

--- sdk/src/utils/v2-helpers.ts
+++ sdk/src/utils/v2-helpers.ts
@@ -46,3 +46,7 @@ export const helperV2_1 = () => { return 9198; };
 /** Utility 2 */
 export const helperV2_2 = () => { return 7912; };
 
+
+/** Utility 3 */
+export const helperV2_3 = () => { return 7929; };
+
